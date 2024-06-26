4f3a5a58d169293808b7535c1d55931f267b105b
adammaster29 <104658788+adammaster29@users.noreply.github.com>
2024-06-25 19:04:46 -0500

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import './App.css';
+import '../Resposive.css'
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null);
+
+  const doctorUser = [
+    { id: 10025645874, nombre: "adam agudelo", fecha: "14/05/2024" },
+    { id: 245874, nombre: "teresa cordoba", fecha: "10/07/2024" },
+    { id: 1010245155, nombre: "paola jimenez", fecha: "12/08/2024" }
+  ];
+
+  useEffect(() => {
+    setUsuarios(doctorUser);
+  }, []);
+
+  const agregar = (data) => {
+    setUsuarios([...usuarios, data]);
+  };
+
+  const eliminar = (id) => {
+    const btnDelete = usuarios.filter(user => user.id !== id);
+    setUsuarios(btnDelete);
+  };
+
+  const actualizar = (editar) => {
+    const index = usuarios.findIndex(user => user.id === usuarioSeleccionado.id);
+    const updatedUsuarios = [...usuarios];
+    updatedUsuarios[index] = editar;
+    setUsuarios(updatedUsuarios);
+  };
+
+  return (
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              actualizar={actualizar}
+              agregar={agregar}
+              usuarioSeleccionado={usuarioSeleccionado}
+              setUsuarioSeleccionado={setUsuarioSeleccionado}
+              eliminar={eliminar}
+              usuarios={usuarios}
+            />
+          }
+        />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,142 @@
+import React, { useEffect, useState } from 'react';
+import { useForm } from "react-hook-form";
+
+const Home = ({ actualizar, agregar, eliminar, usuarioSeleccionado, setUsuarioSeleccionado, usuarios }) => {
+    const [doctor, setDoctor] = useState(1);
+    const [formulario, setFormulario] = useState(false);
+    const { register, handleSubmit, reset, setValue } = useForm();
+
+    // Cambio de opción
+    const change = (option) => {
+        setDoctor(option);
+    };
+
+    // Formulario
+    const formularioPaciente = () => {
+        setFormulario(!formulario);
+    };
+
+    const submit = (data, e) => {
+        e.preventDefault();
+        if (usuarioSeleccionado) {
+            actualizar(data);
+            setUsuarioSeleccionado('');
+        } else {
+            agregar(data);
+        }
+        reset();
+        setFormulario(false);
+    };
+
+    useEffect(() => {
+        if (usuarioSeleccionado) {
+            setValue("id", usuarioSeleccionado?.id);
+            setValue("nombre", usuarioSeleccionado?.nombre);
+            setValue("fecha", usuarioSeleccionado?.fecha);
+        } else {
+            reset(); // Restablecer el formulario si no hay usuario seleccionado
+        }
+    }, [usuarioSeleccionado, setValue, reset]);
+
+    return (
+        <div className='father'>
+            <h1>AGENDAR CITAS</h1>
+            {doctor === 1 &&
+                <div className="hijo">
+                    <div onClick={() => change(2)} className="card__hijo">
+                        <img src="/img/doctor-uno.png" alt="Doctor Erick" />
+                        <p>D. Erick</p>
+                    </div>
+                    <div onClick={() => change(3)} className="card__hijo">
+                        <img src="/img/doctor-dos.png" alt="Doctor Paola" />
+                        <p>D. Paola</p>
+                    </div>
+                    <div onClick={() => change(4)} className="card__hijo">
+                        <img src="/img/doctor-tres.png" alt="Doctor Adams" />
+                        <p>D. Adams</p>
+                    </div>
+                    <div onClick={() => change(5)} className="card__hijo">
+                        <img src="/img/doctor-cuatro.png" alt="Doctor Rafael" />
+                        <p>D. Rafael</p>
+                    </div>
+                    <div onClick={() => change(6)} className="card__hijo">
+                        <img src="/img/doctor-cinco.png" alt="Doctor Antonio" />
+                        <p>D. Antonio</p>
+                    </div>
+                    <div onClick={() => change(7)} className="card__hijo">
+                        <img src="/img/doctor-seis.webp" alt="Doctor Patricia" />
+                        <p>D. Patricia</p>
+                    </div>
+                </div>
+            }
+
+            {[2, 3, 4, 5, 6, 7].includes(doctor) && (
+                <div className="option">
+                    <div className="contenedor__doctores-formulario">
+                        <div className="doctores">
+                            <img className='doctor-agenda' src={`/img/doctor-${doctor === 2 ? 'uno' : doctor === 3 ? 'dos' : doctor === 4 ? 'tres' : doctor === 5 ? 'cuatro' : doctor === 6 ? 'cinco' : 'seis'}.png`} alt={`Doctor ${doctor === 2 ? 'Erick' : doctor === 3 ? 'Paola' : doctor === 4 ? 'Adams' : doctor === 5 ? 'Rafael' : doctor === 6 ? 'Antonio' : 'Patricia'}`} />
+                            <p>{doctor === 2 ? 'Erick Martinez' : doctor === 3 ? 'Paola Jimenez' : doctor === 4 ? 'Adam Agudelo' : doctor === 5 ? 'Rafael Perez' : doctor === 6 ? 'Antonio Mercado' : 'Patricia Teran'}</p>
+
+                            <div onClick={formularioPaciente} className="uiverse">
+                                <span className="tooltip">Citas</span>
+                                <span>Agendar</span>
+                            </div>
+                            <div onClick={() => change(1)} className="uiverse">
+                                <span>Regresar</span>
+                            </div>
+                        </div>
+                        {formulario && (
+                            <div className="padre__formulario">
+                                <form onSubmit={handleSubmit(submit)}>
+                                    <div className="coolinput">
+                                        <label htmlFor="id" className="text">Documento:</label>
+                                        <input type="text" placeholder="Documento..." {...register("id")} className="input" />
+                                    </div>
+                                    <div className="coolinput">
+                                        <label htmlFor="nombre" className="text">Nombre:</label>
+                                        <input type="text" placeholder="Nombre..." {...register("nombre")} className="input" />
+                                    </div>
+                                    <div className="coolinput">
+                                        <label htmlFor="fecha" className="text">Fecha:</label>
+                                        <input type="date" {...register("fecha")} className="input" />
+                                    </div>
+                                    <button type="submit" className="button">Agendar</button>
+                                </form>
+                                <button className="button" onClick={() => setFormulario(false)}>Salir</button>
+                            </div>
+                        )}
+                    </div>
+                    <div className="tabla">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Documento</th>
+                                    <th>Nombre</th>
+                                    <th>Fecha</th>
+                                    <th>----</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {usuarios.map((user, index) => (
+                                    <tr key={user.id}>
+                                        <td>{index + 1}</td>
+                                        <td>{user.id}</td>
+                                        <td>{user.nombre}</td>
+                                        <td>{user.fecha}</td>
+                                        <td className='contenedorBtn' >
+                                            <button className='delete' onClick={() => { eliminar(user.id) }}>Eliminar</button>
+                                            <button className='edite' onClick={() => { setUsuarioSeleccionado(user), formularioPaciente() }}>Editar</button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Home;
